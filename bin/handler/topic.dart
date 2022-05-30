29f525bc76a180728a3644b3b2e6d2637ddfd667
ngohieuphat <ngohipha1@gmail.com>
2022-05-30 16:53:04 +0700

    Initial commit

---
+++ bin/handler/topic.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:shelf/shelf.dart';
+
+import '../data/topic_strorage.dart';
+
+
+Future<Response> topicsHandler(Request req) async {
+  final topics = await topicStorage.load();
+  return Response.ok(
+    jsonEncode(
+      topics.map((e) => e.toJson()).toList(),
+    ),
+  );
+}
